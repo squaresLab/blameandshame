7c7ce10fc349beb6c531b336a32ccc695608b78d
ChrisTimperley <christimperley@googlemail.com>
2017-11-30 13:17:12 -0500

    added stub analyze_fix_commit function

--- collect.py
+++ collect.py
@@ -6,6 +6,25 @@ import argparse
 DESC = "TODO: Add a description of how this tool works."
 
 
+def analyze_fix_commit(repo_url: str,
+                       fix_sha: str) -> None:
+    """
+    Collects historical information for a given bug fix, and writes that
+    information to disk.
+
+    TODO: Don't use this function to write to disk; splitting data extraction
+        and storage into two functions makes testing much easier :-)
+
+    TODO: Assumes that the bug is fixed by a single fix, and not by a sequence
+        of successive commits.
+
+    Args:
+        repo_url:   URL of the repository that hosts the fixed program.
+        fix_sha:    SHA for the bug-fixing commit.
+    """
+    print('hello')
+
+
 def build_parser():
     parser = argparse.ArgumentParser(description=DESC)
     return parser
