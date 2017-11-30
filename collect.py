ef6dff877a82a7f8933fb95d8f3008f576d497d4
ChrisTimperley <christimperley@googlemail.com>
2017-11-30 13:28:34 -0500

    added repo_path function

--- collect.py
+++ collect.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python3
+import os
 import git
 import argparse
+import urlparse
 
 
 DESC = "TODO: Add a description of how this tool works."
+
+# Path to the directory used to hold downloaded Git repositories.
+REPOS_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                         '.repos')
+
+
+def repo_path(repo_url: str) -> str:
+    """
+    Computes the intended path to the local copy of a given repository,
+    specified by its URL.
+    """
+    # get the name of the repo
+    path = urlparse.urlparse(repo_url)
+    path, ext = os.path.splitext(path)
+    _, name = os.path.split(path)
+
+    return os.path.join(REPOS_DIR, name)
 
 
 def analyze_fix_commit(repo_url: str,
@@ -22,7 +41,17 @@ def analyze_fix_commit(repo_url: str,
         repo_url:   URL of the repository that hosts the fixed program.
         fix_sha:    SHA for the bug-fixing commit.
     """
-    print('hello')
+
+    # Use GitPython to clone the entire history for the given repository to
+    # disk. Repositories are cloned to their own subdirectories within
+    # `${PWD}/.repos`.
+    #
+    # Warning: This can potentially consume quite a bit of disk space!
+
+    # Determine the (intended) location of the given repo on disk
+    path = repo_path(repo_url)
+
+    #repo = git.Repo.clone_from()
 
 
 def build_parser():
