f43e11fc6f630046359967e8877895c59f902245
Jeremy Lacomis <jlacomis@cmu.edu>
2017-12-03 15:16:12 -0500

    Docstring for the Change enum

--- src/blameandshame/util.py
+++ src/blameandshame/util.py
@@ -58,6 +58,10 @@ def get_repo(repo_url: str) -> git.Repo:
 
 
 class Change(Enum):
+    """
+    Enum of the possible types of git changes. These values can be used as
+    arguments to the Diff object's iter_change_type method.
+    """
     ADDED = 'A'
     DELETED = 'D'
     MODIFIED = 'M'
