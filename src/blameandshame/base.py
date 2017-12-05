c8204e20fb552fb798bdd2a4f01fcb3684c24cd7
Jeremy Lacomis <jlacomis@cmu.edu>
2017-12-04 20:05:43 -0500

    Slight change from subscripts to unpacking

--- src/blameandshame/base.py
+++ src/blameandshame/base.py
@@ -32,8 +32,8 @@ class Project(object):
         """
         # get the name of the repo
         path = urllib.parse.urlparse(url).path
-        path = os.path.splitext(path)[0]
-        name = os.path.split(path)[1]
+        path, _ = os.path.splitext(path)
+        _, name = os.path.split(path)
 
         return os.path.join(Project.REPOS_DIR, name)
 
