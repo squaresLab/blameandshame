1d84e30d30decdcd1f30862d0ca1f0889540ac6e
ChrisTimperley <christimperley@googlemail.com>
2017-12-05 14:51:49 -0500

    fixed directory used in 'test_conformance'

--- tests/style_test.py
+++ tests/style_test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 import unittest
 import pycodestyle
 
@@ -6,7 +7,9 @@ import pycodestyle
 class StyleTestCase(unittest.TestCase):
     def test_conformance(self):
         style = pycodestyle.StyleGuide()
-        result = style.check_files(['../src/blameandshame'])
+        test_dir = os.path.dirname(os.path.abspath(__file__))
+        src_dir = os.path.join(test_dir, '../src/blameandshame')
+        result = style.check_files([src_dir])
         self.assertEqual(result.total_errors,
                          0,
                          "Found code style errors and/or warnings.")
