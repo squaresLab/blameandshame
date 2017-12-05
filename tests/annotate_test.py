2e03f62a21171fa0f4c9522ec44db468349195cc
ChrisTimperley <christimperley@googlemail.com>
2017-12-04 21:40:20 -0500

    added extra test case to 'test_annotate' (#11)

--- tests/annotate_test.py
+++ tests/annotate_test.py
@@ -2,7 +2,8 @@
 import os
 import unittest
 from blameandshame.base import Project
-from blameandshame.annotate import annotate
+from blameandshame.annotate import  annotate, \
+                                    column_last_commit
 
 
 class AnnotateTestCase(unittest.TestCase):
@@ -16,5 +17,20 @@ class AnnotateTestCase(unittest.TestCase):
             (3, 'How are you? :-)'),
             (4, ''),
             (5, 'Debugging time!')
+        ]
+        self.assertEqual(actual, expected)
+
+        columns = [column_last_commit]
+        actual = \
+            annotate(project,
+                     project.repo.commit("e1d2532"),
+                     "file-one.txt",
+                     columns)
+        expected = [
+            (1, 'Hello world.',     'e1d2532'),
+            (2, '',                 '922e13d'),
+            (3, 'How are you? :-)', '922e13d'),
+            (4, '',                 '0d841d1'),
+            (5, 'Debugging time!',  '0d841d1')
         ]
         self.assertEqual(actual, expected)
