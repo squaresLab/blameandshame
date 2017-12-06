af5b91ef8ff48af2532ed7eb0f64da660dfe15d6
ChrisTimperley <christimperley@googlemail.com>
2017-12-06 13:55:56 -0500

    added simple test to 'test_column_num_commits_to_file_since_commit'

--- tests/annotate_test.py
+++ tests/annotate_test.py
@@ -2,7 +2,8 @@
 import unittest
 from blameandshame.base import Project
 from blameandshame.annotate import  annotate, \
-                                    column_last_commit
+                                    column_last_commit, \
+                                    column_num_commits_to_file_since_commit
 
 
 class AnnotateTestCase(unittest.TestCase):
@@ -43,3 +44,16 @@ class AnnotateTestCase(unittest.TestCase):
 
         project = Project.from_url('https://github.com/squaresLab/blameandshame-test-repo')
         check_one(project, 'e1d2532', 'file-one.txt', 1, 'e1d2532')
+
+
+    def test_column_num_commits_to_file_since_commit(self):
+        def check_one(project, commit, filename, line, expected):
+            commit = project.repo.commit(commit)
+            num_commits = column_num_commits_to_file_since_commit(project,
+                                                                  commit,
+                                                                  filename,
+                                                                  line)
+            self.assertEqual(num_commits, expected)
+
+        project = Project.from_url('https://github.com/squaresLab/blameandshame-test-repo')
+        check_one(project, 'e1d2532', 'file-one.txt', 1, 0)
