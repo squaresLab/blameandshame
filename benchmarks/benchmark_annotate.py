ad7a4b3428eb5b7007a182f0e6d961128326beed
ChrisTimperley <christimperley@googlemail.com>
2017-12-06 17:47:34 -0500

    added benchmark case for 'annotate' function (#30)

---
+++ benchmarks/benchmark_annotate.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3.6
+import cProfile
+from blameandshame.base import Project
+from blameandshame.annotate import annotate, \
+                                   column_last_commit, \
+                                   column_num_file_commits_after_modified, \
+                                   column_num_project_commits_after_modified, \
+                                   column_num_days_since_modified
+
+def benchmark_annotate_closure():
+    project = Project.from_url('https://github.com/google/closure-compiler')
+    fix_sha = '1dfad50'
+    commit = project.repo.commit('{}~1'.format(fix_sha))
+    filename = 'src/com/google/javascript/jscomp/RemoveUnusedVars.java'
+
+    cols = [
+        column_last_commit,
+        column_num_file_commits_after_modified,
+        column_num_project_commits_after_modified,
+        column_num_days_since_modified
+    ]
+
+    annotate(project, commit, filename, cols)
+
+
+if __name__ == '__main__':
+    cProfile.run('benchmark_annotate_closure()')
