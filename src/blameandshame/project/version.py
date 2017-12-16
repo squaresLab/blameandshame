f9b6698802ad58300a62b9aef6a59e503e515cd0
ChrisTimperley <christimperley@googlemail.com>
2017-12-16 08:51:26 -0500

    added stub 'tests' property to ProjectVersion (#54)

--- src/blameandshame/project/version.py
+++ src/blameandshame/project/version.py
@@ -17,8 +17,21 @@ class ProjectVersion(object):
 
     @property
     def project(self) -> Project:
+        """
+        The project associated with this version.
+        """
         return self.__project
 
     @property
     def commit(self) -> git.Commit:
+        """
+        The commit associated with this version of the project.
+        """
         return self.__commit
+
+    @property
+    def tests(self) -> 'TestSuite':
+        """
+        The test suite used by this project version.
+        """
+        raise NotImplementedError
