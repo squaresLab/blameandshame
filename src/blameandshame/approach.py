6dcc625524bb31dd3f928584dd4cb99ae0cdff57
Jeremy Lacomis <jlacomis@cmu.edu>
2017-12-16 15:37:40 -0500

    Add imports and docstrings

--- src/blameandshame/approach.py
+++ src/blameandshame/approach.py
@@ -1,4 +1,25 @@
-class Approach(object):
+from blameandshame.project import Project
+from blameandshame.base import Commits
+from blameandshame.component import Component
+from blameandshame.localization import Localization
+import git
+from typing import List
 
+
+class Approach(object):
+    """
+    An abstract base class for fault localization approaches.
+    """
     def __init__(self) -> None:
-        pass
+        raise NotImplementedError
+
+    def localize(self,
+                 project: Project,
+                 commit: git.Commit,
+                 granularity: Component,
+                 scope: List[Component]) -> Localization:
+        """
+        Apply the specific fault localization approach and return a
+        Localization object.
+        """
+        raise NotImplementedError("define localize to use this base class")
