c368ab0038672e144b8b1a40e449f69e6c5fa22e
Jeremy Lacomis <jlacomis@cmu.edu>
2017-12-16 15:49:31 -0500

    Add Component and Localization stubs, add test file.

--- src/blameandshame/approach.py
+++ src/blameandshame/approach.py
@@ -1,18 +1,31 @@
 from blameandshame.project import Project
 from blameandshame.base import Commits
-from blameandshame.component import Component
-from blameandshame.localization import Localization
+# from blameandshame.component import Component
+# from blameandshame.localization import Localization
 import git
 from typing import List
+
+
+class Component():
+    """
+    Stub Component. Delete and uncomment the import above when the full
+    version is complete
+    """
+    pass
+
+
+class Localization():
+    """
+    Stub Localization. Delete and uncomment the import above when the full
+    version is complete
+    """
+    pass
 
 
 class Approach(object):
     """
     An abstract base class for fault localization approaches.
     """
-    def __init__(self) -> None:
-        raise NotImplementedError
-
     def localize(self,
                  project: Project,
                  commit: git.Commit,
