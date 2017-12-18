8f7f0159f68c8df735d97da26fbde37ef8590a14
Jeremy Lacomis <jlacomis@cmu.edu>
2017-12-18 14:03:51 -0500

    Fix minor stuff in components.py

--- src/blameandshame/components.py
+++ src/blameandshame/components.py
@@ -4,36 +4,32 @@ from typing import Dict, FrozenSet, List, Tuple, Optional, Set
 class Component(object):
 
     def __init__(self):
-        pass
+        raise NotImplementedError
 
 
 class File_C(Component):
 
-    file_name = ""
-
-    def __init__(self, filename: str = ""):
+    def __init__(self,
+                 filename: str = "") -> None:
         self.file_name = filename
 
 
 class Line_C(Component):
 
-    lineno = 0
-    filename = ""
-
-    def __init__(self, filename: str = "", lineno: int = 0):
+    def __init__(self,
+                 filename: str = "",
+                 lineno: int = 0) -> None:
         self.filename = filename
         self.lineno = lineno
 
 
 class Function_C(Component):
     
-    filename = None
-    func_name= ""
-    line_start = 0
-    line_end = 0
-    
-    def __init__(self, funcname: str = "", filename: File_C = None,
-                 linestart: int = 0, lineend: int = 0):
+    def __init__(self,
+                 funcname: str = "",
+                 filename: File_C = None,
+                 linestart: int = 0,
+                 lineend: int = 0) -> None:
         self.func_name = funcname
         self.filename = filename
         self.line_start = linestart
