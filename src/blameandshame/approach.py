0e45dcb671cdf43c6b24a8d057a15142fa6ecff2
Jeremy Lacomis <jlacomis@cmu.edu>
2017-12-18 14:41:56 -0500

    Import correct stuff

--- src/blameandshame/approach.py
+++ src/blameandshame/approach.py
@@ -1,24 +1,8 @@
 from blameandshame.project import Project
-# from blameandshame.component import Component
-# from blameandshame.localization import Localization
+from blameandshame.components import Component
+from blameandshame.localization import Localization
 import git
 from typing import List
-
-
-class Component():
-    """
-    Stub Component. Delete and uncomment the import above when the full
-    version is complete
-    """
-    pass
-
-
-class Localization():
-    """
-    Stub Localization. Delete and uncomment the import above when the full
-    version is complete
-    """
-    pass
 
 
 class Approach(object):
