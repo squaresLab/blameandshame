5dd5a45428d0bf04676e1688fddc682d42bb166a
Jeremy Lacomis <jlacomis@cmu.edu>
2017-12-18 19:04:37 -0500

    More whitespace

--- tests/localization_test.py
+++ tests/localization_test.py
@@ -4,7 +4,6 @@ import unittest
 
 class LocalizationTestCase(unittest.TestCase):
     pass
-
 #     def check_one(file_name, expected):
 # """
 #         loc_res = Localization.from_file(file_name)
@@ -14,9 +13,9 @@ class LocalizationTestCase(unittest.TestCase):
 #     mapping = {Line_C("File1.java", 3): 0.3,
 #                Line_C("File1.java", 5): 0.5}
 #     expected = Localization(mapping, scope)
-    
 #     check_one("filename.yaml", expected)
 # """
+
 
 if __name__ == '__main__':
     unittest.main()
